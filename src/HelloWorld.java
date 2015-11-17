47d7fababe7ca8c693647652f861a62e935f7cdf
kouno takanobu <legendskyworld@gmail.com>
2015-11-17 17:22:35 +0900

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+     System.out.println("2015.11.16");
+     System.out.println("HelloWorld");
+	}
+
+}
